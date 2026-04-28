277dee3ca4e9af8f5c17f5723b16dc9f265c741a
obox56 <n11958057@qut.edu.au>
2026-04-29 09:47:40 +1000

    Add very basic JSON reading to get total hours played for a directory.

--- src/main/java/com/boxy/mcworldstats/model/WorldDataReader.java
+++ src/main/java/com/boxy/mcworldstats/model/WorldDataReader.java
@@ -1,5 +1,8 @@
 package com.boxy.mcworldstats.model;
 
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,17 +13,51 @@ public class WorldDataReader {
     public static void GetDirectoryStatistics(File directory) {
         if (!directory.isDirectory()) throw new IllegalArgumentException("File must be a directory!");
 
+        double directoryHrs = 0.0;
+
         try (Stream<Path> paths = Files.walk(directory.toPath(),1)){
-            paths.filter(Files::isDirectory)
-                    .forEach(WorldDataReader::GetPerSaveStats);
+            Path[] saves = paths.filter(Files::isDirectory).toArray(Path[]::new);
+            for(Path save : saves) {
+                directoryHrs += GetPerSaveStats(save);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        System.out.println("Total Hrs for given directory: "+directoryHrs);
     }
 
-    private static void GetPerSaveStats(Path saveFolder) {
+    private static double GetPerSaveStats(Path saveFolder) {
         Path statsFolder = saveFolder.resolve("players/stats").normalize();
-        String longname = statsFolder.toAbsolutePath().toString();
-        System.out.println(longname);
+        File fileEq = statsFolder.toFile();
+
+        double worldHrs = 0.0;
+
+        // only get stats for stats directories that actually exist
+        if (fileEq.isDirectory() && fileEq.exists()) {
+//            String longname = statsFolder.toAbsolutePath().toString();
+//            System.out.println(longname);
+
+            try (Stream<Path> individual_stat = Files.walk(statsFolder,1)) {
+               Path[] statFiles = individual_stat
+                       .filter(Files::isRegularFile)
+                       .filter(p -> p.getFileName().toString().toLowerCase().endsWith(".json"))
+                       .toArray(Path[]::new);
+               for (Path stat : statFiles) {
+                   worldHrs += GetPlayerHours(stat);
+               }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return worldHrs;
+    }
+
+    private static double GetPlayerHours(Path statFile) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(statFile);
+        int time_played_ticks = root.get("stats").get("minecraft:custom").get("minecraft:play_time").asInt(0);
+
+        return (double)time_played_ticks/72000;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.boxy.mcworldstats {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires tools.jackson.databind;
 
     opens com.boxy.mcworldstats to javafx.fxml;
     exports com.boxy.mcworldstats;
