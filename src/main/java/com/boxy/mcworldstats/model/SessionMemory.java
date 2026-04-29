1d5a9d701c16b131b58eae7e01fd3bffd7194c0e
obox56 <n11958057@qut.edu.au>
2026-04-30 09:12:01 +1000

    Finish updating docs for SessionMemory.

--- src/main/java/com/boxy/mcworldstats/model/SessionMemory.java
+++ src/main/java/com/boxy/mcworldstats/model/SessionMemory.java
@@ -18,8 +18,8 @@ public class SessionMemory {
 
     /**
      * Get a specific player from the Session Memory. Returns a new player with the given UUID if it doesn't exist.
-     * @param raw_uuid
-     * @return
+     * @param raw_uuid The UUID of the player to get
+     * @return the existing Player, or a new Player with the given UUID if not exists.
      */
     public static Player getPlayer(String raw_uuid) {
         if (players.containsKey(raw_uuid)) {
@@ -29,6 +29,10 @@ public class SessionMemory {
         }
     }
 
+    /**
+     * Store a Player to the Session Memory. Creates a new entry if it doesn't exist.
+     * @param new_player the Player to store.
+     */
     public static void setPlayer(Player new_player) {
         players.put(new_player.getUUID(), new_player);
     }
